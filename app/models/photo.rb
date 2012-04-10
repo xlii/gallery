d333457b0faad40e467ecadcc9da40d5c730bded
Adriana Silva <drica.silva@gmail.com>
2012-04-10 16:05:33 -0300

    adicionado tamanho large as photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,4 @@
 class Photo < ActiveRecord::Base
   belongs_to :gallery
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :large => "700x700>", :medium => "300x300>", :thumb => "100x100>" }
 end
